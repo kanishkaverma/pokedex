88086ef9bfb8ba4c63eda113db6d01af962fc932
kanishka verma <kanishkaverma@users.noreply.github.com>
2021-01-14 06:20:59 +0530

    Cleaned the code and added navbar.ಠ_ಠ

--- src/components/Pokefind.jsx
+++ src/components/Pokefind.jsx
@@ -5,156 +5,137 @@ import { useSelector } from 'react-redux';
 import Pokeoption from './Pokeoption';
 import axios from 'axios';
 import Loading_svg from './Loading_svg';
-import logo from '../../build/assets/logo.svg';
 import keys from '../../build/assets/arrow_keys.svg';
+import Navbar from './Navbar';
+import { setTimeout } from 'core-js';
 
 const Pokefind = () => {
-    const pokedata = useSelector((state) => state);
-    const data = useSelector((state) => state.delta);
-    const dispatch = useDispatch();
+  const { data, pokelist, loading, color } = useSelector((state) => state.delta);
+  const [input, setInput] = useState('');
+  const [loading_local, setLoadingLocal] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [error, setError] = useState(null);
+  const [visible, setvisible] = useState(true);
+  const ref = useRef(null);
+  const dispatch = useDispatch();
 
-    /*
-     * @type {React.MutableRefObject<HTMLInputElement>}
-     */
-    const ref = useRef(null);
+  // fall color back for default theme
+  var fallback = 'white';
+  // api call which fetches pokemon list and updates the global state.
 
-    const [input, setInput] = useState('');
-    const [loading_local, setLoadingLocal] = useState(false);
-    const [index, setIndex] = useState(0);
-    const pokelist = useSelector((state) => state.delta.pokelist);
-    const [error, setError] = useState(null);
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000').then((res) => {
+      dispatch(setPokelist(res.data.results));
+    });
 
-    // const invertHex = (hex) => (Number(`0x1${hex}`) ^ 0xffffff).toString(16).substr(1).toUpperCase();
+    document.addEventListener('keydown', function (e) {
+      switch (e.keyCode) {
+        case 37:
+          setIndex((prev) => prev - 1);
 
-    // invertHex('00FF00'); // Returns FF00FF
+          break;
+        case 38:
+          //   alert('up');
+          break;
+        case 39:
+          setIndex((prev) => prev + 1);
+          break;
+        case 40:
+          break;
+      }
+    });
+  }, []);
 
-    // fall color back for default theme
-    var fallback = 'white';
-    // api call which fetches pokemon list and updates the global state.
+  useEffect(() => {
+    if (pokelist && pokelist.length > 800) {
+      dispatch(fetchData(pokelist[index].name));
+      ref.current.value = pokelist[index].name;
+    }
+  }, [index]);
 
-    useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000').then((res) => {
-            dispatch(setPokelist(res.data.results));
-        });
+  //Display random pokemon from pokelist in globalstore.
 
-        document.addEventListener('keydown', function (e) {
-            switch (e.keyCode) {
-                case 37:
-                    setIndex((prev) => prev - 1);
+  useEffect(() => {
+    if (pokelist && pokelist.length > 800) {
+      const randomInt = Math.floor(Math.random() * pokelist.length);
+      setIndex(randomInt);
+    }
+  }, [pokelist]);
 
-                    break;
-                case 38:
-                    alert('up');
-                    break;
-                case 39:
-                    setIndex((prev) => prev + 1);
-                    break;
-                case 40:
-                    break;
-            }
-        });
-    }, []);
+  // refresh theme when there is a pokemon in the state
+  useEffect(() => {
+    if (data?.color?.vibrant) {
+      console.log(data);
+      fallback = color.darkVibrant;
+    }
+  }, [data]);
 
-    useEffect(() => {
-        if (pokelist && pokelist.length > 800) {
-            dispatch(fetchData(pokelist[index].name));
-            // setInput(pokelist[index].name);
-            ref.current.value = pokelist[index].name;
-        }
-    }, [index]);
+  useEffect(() => {
+    setInput(input.trim().toLowerCase());
+  }, [input]);
+  // click handler:
+  // sets index to current pokemon
+  // dispatches fetch data
+  // sets loading  true and after a delay sets it false
+  const handleClick = (e) => {
+    const currentIndex = pokelist.findIndex((element) => element.name == input);
+    if (currentIndex == -1) {
+      setError('Pokemon not found');
+    } else {
+      setError(null);
+      setIndex(currentIndex);
+      setLoadingLocal(true);
+      dispatch(fetchData(input));
+      setLoadingLocal(false);
+    }
+  };
+  const handleEnter = (e) => {
+    if (e.keyCode == 13) {
+      handleClick();
+    }
+  };
 
-    //Display random pokemon from pokelist in globalstore.
+  const visibleUseEffect = useEffect(() => {
+    setTimeout(() => setvisible(false), 3000);
+  }, [visible]);
 
-    useEffect(() => {
-        if (pokelist && pokelist.length > 800) {
-            const randomInt = Math.floor(Math.random() * pokelist.length);
-            setIndex(randomInt);
-        }
-    }, [pokelist]);
+  return (
+    <div className='main'>
+      <Navbar />
 
-    // refresh theme when there is a pokemon in the state
-    useEffect(() => {
-        if (data?.color?.vibrant) {
-            // console.log(invertHex(data.color.vibrant.slice(1)));
-            console.log(data.color);
-            fallback = data.color.darkVibrant;
-        }
-    }, [data]);
-
-    useEffect(() => {
-        setInput(input.trim().toLowerCase());
-    }, [input]);
-    // click handler:
-    // sets index to current pokemon
-    // dispatches fetch data
-    // sets loading  true and after a delay sets it false
-    const handleClick = (e) => {
-        const currentIndex = pokelist.findIndex((element) => element.name == input);
-        if (currentIndex == -1) {
-            setError('Pokemon not found');
-        } else {
-            setError(null);
-            setIndex(currentIndex);
-            // setInput(input.trim().toLowerCase())
-            dispatch(fetchData(input));
-            setLoadingLocal(true);
-            setInterval(() => {
-                setLoadingLocal(false);
-            }, 1000);
-        }
-    };
-    const handleEnter = (e) => {
-        if (e.keyCode == 13) {
-            handleClick();
-        }
-    };
-
-    return (
-        <div>
-            <div className='icon'>
-                <img src={logo} alt='' />
-            </div>
-
-            <div className='search-bar-root'>
-                {/* style theme tag according to the pokemon  */}
-                <input
-                    type='text'
-                    // value={input}
-                    ref={ref}
-                    onChange={(i) => setInput(i.target.value)}
-                    className='search-bar'
-                    style={{
-                        // use data.color if available otherwise use fallback color.
-                        border: `4px solid ${data?.color?.darkVibrant ? data.color.darkVibrant : fallback}`,
-                        backgroundColor: `${data?.color?.lightMuted ? data.color.lightMuted : fallback}`,
-                        color: `${data?.color?.darkMuted ? data.color.darkMuted : fallback}`
-                    }}
-                    onKeyDown={handleEnter}
-                />
-            </div>
-            <div className='response'>
-                {/* if pokedata is empty or loading_local is true then display empty otherwise render pokeoption  */}
-                {Object.keys(pokedata).length == 0 || loading_local ? (
-                    // {tr  ue ? (
-                    <Loading_svg />
-                ) : pokedata.delta.error || error ? (
-                    <h2>{pokedata.delta.error || error}</h2>
-                ) : (
-                    <div className='Pokeoption-root'>
-                        <Pokeoption />
-                    </div>
-                )}
-            </div>
-            <div className='keys-container'>
-                <img src={keys} alt='' />
-            </div>
-
-            {/* <div className='keys-container'>
-                <div>left</div>
-                <div>right</div>
-            </div> */}
-        </div>
-    );
+      <div className='search-bar-root'>
+        {/* style theme tag according to the pokemon  */}
+        <input
+          type='text'
+          // value={input}
+          ref={ref}
+          onChange={(i) => setInput(i.target.value)}
+          className='search-bar'
+          style={{
+            // use data.color if available otherwise use fallback color.
+            border: `4px solid ${color?.darkVibrant ? color.darkVibrant : fallback}`,
+            backgroundColor: `${color?.lightMuted ? color.lightMuted : fallback}`,
+            color: `${color?.darkMuted ? color.darkMuted : fallback}`
+          }}
+          onKeyDown={handleEnter}
+        />
+      </div>
+      <div className='response'>
+        {/* if pokedata is empty or loading_local is true then display loading spinner otherwise render pokeoption  */}
+        {loading ? (
+          // {tr  ue ? (
+          <Loading_svg />
+        ) : error || error ? (
+          <h2>{error || error}</h2>
+        ) : (
+          <div className='Pokeoption-root'>
+            <Pokeoption />
+          </div>
+        )}
+      </div>
+      <div className='keys-container'>{visible && <img src={keys} alt='' />}</div>
+    </div>
+  );
 };
 
 export default Pokefind;
--- src/components/Pokeoption.jsx
+++ src/components/Pokeoption.jsx
@@ -1,52 +1,52 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { fetchData } from "../redux/actions/actions";
-import { useSelector } from "react-redux";
-import Pokemon from "./Pokemon";
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchData } from '../redux/actions/actions';
+import { useSelector } from 'react-redux';
+import Pokemon from './Pokemon';
 
-import { usePalette } from "react-palette";
-import { setColor } from "../redux/actions/actions";
+import { usePalette } from 'react-palette';
+import { setColor } from '../redux/actions/actions';
 
 function Pokeoption() {
-  //destructuring
-  const imgsrc = useSelector((state) => state.delta.data.sprites.front_default);
-  const name = useSelector((state) => state.delta.data.name);
-  const color = useSelector((state) => state.delta.color);
-  const dispatch = useDispatch();
-  const [pokerender, setPokerender] = useState(false);
-  const [pokeOptionrender, setPokeOptionrender] = useState(true);
-  const { data, loading, error } = usePalette(imgsrc);
+    //destructuring
+    const imgsrc = useSelector((state) => state.delta.data.sprites.front_default);
+    const name = useSelector((state) => state.delta.data.name);
+    const color = useSelector((state) => state.delta.color);
+    const dispatch = useDispatch();
+    const [pokerender, setPokerender] = useState(false);
+    const [pokeOptionrender, setPokeOptionrender] = useState(true);
+    const { data, loading, error } = usePalette(imgsrc);
 
-  //click handler
-  const handleClick = (e) => {
-    setPokerender(true);
-    setPokeOptionrender(false);
-  };
-  // dynamically change theme
-  useEffect(() => {
-    if (!loading) {
-      if (error) {
-        console.log("Err");
-      } else {
-        dispatch(setColor(data));
-        document.body.style.backgroundColor = data.vibrant;
-      }
-    }
-  }, [loading]);
+    //click handler
+    const handleClick = (e) => {
+        setPokerender(true);
+        setPokeOptionrender(false);
+    };
+    // dynamically change theme
+    useEffect(() => {
+        if (!loading) {
+            if (error) {
+                console.log('Err');
+            } else {
+                dispatch(setColor(data));
+                document.body.style.backgroundColor = data.vibrant;
+            }
+        }
+    }, [loading]);
 
-  return (
-    <div>
-      {/* conditional rendering of Pokemon component.   */}
-      {pokeOptionrender && (
-        <div className="pokemon-select">
-          <img src={imgsrc} alt="Pokemon image." onClick={handleClick} />
-          <h2>{name}</h2>
+    return (
+        <div className='pokeoption-root'>
+            {/* conditional rendering of Pokemon component.   */}
+            {pokeOptionrender && (
+                <div className='pokemon-select'>
+                    <img src={imgsrc} alt='Pokemon image.' onClick={handleClick} />
+                    <h2>{name}</h2>
+                </div>
+            )}
+
+            <div className='pokeoption-pokemon'>{pokerender && <Pokemon />}</div>
         </div>
-      )}
-
-      <div className="pokeoption-pokemon">{pokerender && <Pokemon />}</div>
-    </div>
-  );
+    );
 }
 export default Pokeoption;
