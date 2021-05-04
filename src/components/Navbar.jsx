948e8675d8afeca425911281ff62e1d398aa1e24
kanishkaverma <kanisverma@gmail.com>
2021-05-04 08:20:36 +0530

    test-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,21 @@ import '../sass/components/navbar.scss';
 import { VscGithub } from 'react-icons/vsc';
 
 const navbar = () => {
-    return (
-        <header className='nav_header'>
-            <nav className='nav'>
-                <img src={logo} alt='' />
-                <ul className='nav_links'>
-                    <li>
-                        <a href='https://github.com/kanishkaverma/pokedex' target='_blank' className='nav_item'>
-                            <VscGithub size={30} className='git_icon' />
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className='nav_header'>
+      {/* <add-comment></add-comment> */}
+      <nav className='nav'>
+        <img src={logo} alt='' />
+        <ul className='nav_links'>
+          <li>
+            <a href='https://github.com/kanishkaverma/pokedex' target='_blank' className='nav_item'>
+              <VscGithub size={30} className='git_icon' />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default navbar;
